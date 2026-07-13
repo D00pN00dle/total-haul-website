3cc16834a41afe437dc3f76732cd2357c0a1ec1d
d00pn00dl3 <hypemodedesigns@protonmail.com>
2026-07-13 11:21:01 -0400

    hooks.server.js added

---
+++ hooks.server.js
@@ -0,0 +1,20 @@
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  const response = await resolve(event);
+
+  response.headers.set(
+    'Content-Security-Policy',
+    [
+      "default-src 'self'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com",
+      "style-src 'self' 'unsafe-inline'",
+      "img-src 'self' data: https:",
+      "connect-src 'self' https://challenges.cloudflare.com",
+      "frame-src https://challenges.cloudflare.com",
+      "object-src 'none'",
+      "base-uri 'self'",
+    ].join('; ')
+  );
+
+  return response;
+}
